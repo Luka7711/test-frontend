a7bbf446d2df347f068fcfa50117048d1d14a54e
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-10-11 18:06:11 -0500

    style

--- src/components/Inputs/InputCar.js
+++ src/components/Inputs/InputCar.js
@@ -1,15 +1,28 @@
 import PhotoBox from "./PhotoBox";
 
 const InputCar = ({ inputs }) => {
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   let contentList = inputs.map((property, index) => {
     let inputForms = property.inputs.map((element, i) => {
+      console.log(property, "property");
       return (
         <div key={i + 10}>
-          <input
-            className="input-all"
-            type={element.type}
-            placeholder={element.input}
-          />
+          {property.title !== "Description" ? (
+            <input
+              className="input-all"
+              type={element.type}
+              placeholder={element.input}
+              onChange={handleChange}
+            />
+          ) : (
+            <textarea
+              className="textarea"
+              placeholder="Write about the product"
+            />
+          )}
         </div>
       );
     });
